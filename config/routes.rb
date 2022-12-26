c9210ad359cb701a852b8519ce25839aadb868af
Julie Reynal <rjulie.pro@gmail.com>
2022-12-26 22:41:54 +0100

    review model and migration

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
   resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
