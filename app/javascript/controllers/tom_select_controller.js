7fb5dd45114073ebb1bc419d024b965d4258706c
Julie Reynal <rjulie.pro@gmail.com>
2022-12-26 22:33:42 +0100

    tom select on the modal

---
+++ app/javascript/controllers/tom_select_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+import TomSelect from "tom-select"
+
+// Connects to data-controller="tom-select"
+export default class extends Controller {
+  connect() {
+    // console.log("Hello");
+    // new TomSelect(this.element, {})
+
+  }
+}
